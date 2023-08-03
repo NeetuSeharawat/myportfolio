0219541e948f1db4e0924479e9a695d81d70b0cd
neetuSeharawat <neetumorbca@gmail.com>
2023-08-03 19:17:10 +0530

    fifth commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
-
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_hdue83d",
+        "template_0m5l065",
+        form.current,
+        "Ya_6TBxAoqDIYFpwh"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
-      <div class="contact" id="contact">
+    <div class="contact" id="contact">
       <h2>Get In Touch With Me</h2>
       <div class="contact-form-container">
         <div class="contact-form">
-          <form action="https://formspree.io/f/xyylngw" method="POST">
+         
+          <form ref={form} onSubmit={sendEmail}>
             <div class="form-control">
               <label for="name">Name</label>
               <input
                 type="text"
                 id="name"
-                name="sender-name"
+                name="user_name"
                 placeholder="Enter Your Name"
                 class="input-field"
                 required
@@ -24,7 +47,7 @@ const Contact = () => {
               <input
                 type="email"
                 id="email"
-                name="sender-email"
+                name="user_email"
                 placeholder="Enter Your Email"
                 class="input-field"
                 required
@@ -42,17 +65,18 @@ const Contact = () => {
                 required
               ></textarea>
             </div>
-            <input
+            <button
               type="submit"
               value="Submit"
               id="submit-btn"
-              class="submit-btn"
-            />
+              class="submit-btn">
+              Send Message
+            </button>
           </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ExpenseTracker from "../assets/ExpenseTracker.jpg";
 import Ecommerce from "../assets/Ecommerce.jpg";
-import Mailbox from "../assets/Mailbox.jpg";
+import Mailbox from "../assets/MailBox.jpg";
 
 const Projects = () => {
   return (
