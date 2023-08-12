835226d32c47329546121681a1393dc512eda683
Neetu Seharawat <116377226+NeetuSeharawat@users.noreply.github.com>
2023-08-12 16:19:14 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,43 +9,43 @@ import bootstrap from "../assets/bootstrap.png";
 const Skills = () => {
   return (
     <div>
-      <div class="skills" id="skills">
-      <h2 class="skill-header">My Top Skills</h2>
+      <div className="skills" id="skills">
+      <h2 className="skill-header">My Top Skills</h2>
 
-      <div class="skills-wrapper">
-        <div class="first-set animate__animated animate__pulse">
+      <div className="skills-wrapper">
+        <div className="first-set animate__animated animate__pulse">
           <img
             src={react}
             alt=""
             loading="lazy"
-            class="icon icon-card"
+            className="icon icon-card"
           />
           <img
             src={javascript}
             alt=""
             loading="lazy"
-            class="icon icon-card"
+            className="icon icon-card"
           />
           <img
             src={html}
             alt=""
             loading="lazy"
-            class="icon icon-card"
+            className="icon icon-card"
           />
         </div>
 
-        <div class="second-set animate__animated animate__pulse">
+        <div className="second-set animate__animated animate__pulse">
           <img
             src={css}
             alt=""
             loading="lazy"
-            class="icon icon-card"
+            className="icon icon-card"
           />
           <img
             src={bootstrap}
             alt=""
             loading="lazy"
-            class="icon icon-card"
+            className="icon icon-card"
           />
           
         </div>
